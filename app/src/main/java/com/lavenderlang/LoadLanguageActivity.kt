ed3a8d5def5de4ec3e2bbb737a9ddb3dc1080282
Elizabeth <arisovaliza@gmail.com>
2024-04-16 15:16:30 +0300

    minor changes: load languages zagotovka

--- app/src/main/java/com/lavenderlang/LoadLanguageActivity.kt
+++ app/src/main/java/com/lavenderlang/LoadLanguageActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.lavenderlang.backend.dao.language.LanguageDao
 import com.lavenderlang.backend.dao.language.LanguageDaoImpl
@@ -28,13 +29,20 @@ class LoadLanguageActivity: AppCompatActivity(){
     override fun onResume() {
         super.onResume()
 
+        val buttonFirst: Button = findViewById(R.id.buttonFirst)
+        val spinnerPath: Spinner = findViewById(R.id.spinnerPath)
         val editTextPath: EditText = findViewById(R.id.editTextPath)
         val buttonOpen: Button = findViewById(R.id.buttonOpen)
         val languageDao: LanguageDao = LanguageDaoImpl()
 
+        buttonFirst.setOnClickListener {
+            //что-то сделать
+        }
         buttonOpen.setOnClickListener {
-            val path = editTextPath.text.toString()
-            languageDao.getLanguageFromFile(path, this)
+            var path = editTextPath.text.toString()
+            var pathPositionSpinner = spinnerPath.selectedItemPosition
+            //что-то сделать для загрузки
+
             val intent = Intent(this, LanguageActivity::class.java)
             intent.putExtra("lang", languages.keys.toMutableList().last())
             startActivity(intent)
