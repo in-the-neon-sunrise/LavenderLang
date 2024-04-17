c6940db6f5dde271e72671c71bde14e5d61755ec
makar-with-tea <maramakarevich@mail.ru>
2024-04-17 21:50:33 +0300

    minor changes: pretty language buttons

--- app/src/main/java/com/lavenderlang/frontend/MainActivity.kt
+++ app/src/main/java/com/lavenderlang/frontend/MainActivity.kt
@@ -7,15 +7,27 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts.CreateDocument
 import androidx.appcompat.app.AppCompatActivity
 import com.anggrayudi.storage.SimpleStorageHelper
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
 import com.lavenderlang.R
+import com.lavenderlang.backend.dao.language.DictionaryDaoImpl
+import com.lavenderlang.backend.dao.language.GrammarDaoImpl
 import com.lavenderlang.backend.dao.language.LanguageDaoImpl
+import com.lavenderlang.backend.dao.language.PunctuationDaoImpl
+import com.lavenderlang.backend.dao.language.WritingDaoImpl
+import com.lavenderlang.backend.entity.help.Attributes
+import com.lavenderlang.backend.entity.help.CharacteristicEntity
+import com.lavenderlang.backend.entity.help.MascEntity
+import com.lavenderlang.backend.entity.help.PartOfSpeech
+import com.lavenderlang.backend.entity.help.TransformationEntity
 import com.lavenderlang.backend.entity.language.LanguageEntity
+import com.lavenderlang.backend.entity.rule.GrammarRuleEntity
+import com.lavenderlang.backend.entity.word.AdjectiveEntity
+import com.lavenderlang.backend.entity.word.AdverbEntity
+import com.lavenderlang.backend.entity.word.NounEntity
+import com.lavenderlang.backend.entity.word.VerbEntity
 
 var languages : MutableMap<Int, LanguageEntity> = mutableMapOf()
 var nextLanguageId : Int = 0
@@ -44,7 +56,6 @@ class MainActivity : AppCompatActivity() {
         setInstance(this)
         storageHelper = SimpleStorageHelper(this)
 
-        Log.d("meowmeow", languages.keys.toString())
 
         if (!Python.isStarted()) Python.start(AndroidPlatform(this))
 
@@ -53,99 +64,89 @@ class MainActivity : AppCompatActivity() {
 
 
 
+        /*val languageHandler = LanguageDaoImpl()
+        languageHandler.createLanguage("Пример языка", "Пример :>")
+        Log.d("create lang", languages.toString())
+        val writingHandler = WritingDaoImpl()
+        writingHandler.changeVowels(languages[0]!!, "a")
+        writingHandler.changeConsonants(languages[0]!!, "b c d")
+        val dictionaryHandler = DictionaryDaoImpl()
 
+        val word1 = NounEntity(
+            0,
+            "aaa",
+            "кошечка",
+            immutableAttrs = mutableMapOf(Attributes.GENDER to 1)
+        )
+        val word2 = VerbEntity(
+            0,
+            "bbb",
+            "заплакать"
+        )
+        val word3 = AdjectiveEntity(
+            0,
+            "ccc",
+            "красивый"
+        )
+        val word4 = AdverbEntity(
+            0,
+            "ddd",
+            "красиво"
+        )
+        dictionaryHandler.addWord(languages[0]!!.dictionary, word1)
+        dictionaryHandler.addWord(languages[0]!!.dictionary, word2)
+        dictionaryHandler.addWord(languages[0]!!.dictionary, word3)
+        dictionaryHandler.addWord(languages[0]!!.dictionary, word4)
 
-        /*if (languages.isEmpty()) {
-            LanguageDaoImpl().createLanguage("Пример языка", "Пример :>")
-            val dict = DictionaryDaoImpl()
-            val word1 = NounEntity(
-                0,
-                "aaa",
-                "кошечка",
-                immutableAttrs = mutableMapOf(Attributes.GENDER to 1),
-                partOfSpeech = PartOfSpeech.NOUN
+        val grammarHandler = GrammarDaoImpl()
+        val rule = GrammarRuleEntity(0,
+            MascEntity(
+                PartOfSpeech.NOUN, mutableMapOf(Attributes.GENDER to 1)
+            ),
+            mutableMapOf(Attributes.NUMBER to 1, Attributes.CASE to 0),
+            TransformationEntity(0, 1, "", "b")
+        )
+        grammarHandler.addGrammarRule(languages[0]!!.grammar, rule)
+        val rule1 = GrammarRuleEntity(
+            0, MascEntity(
+                PartOfSpeech.VERB, mutableMapOf()
+            ), mutableMapOf(
+                Attributes.NUMBER to 1,
+                Attributes.PERSON to 0,
+                Attributes.TIME to 0,
+                Attributes.GENDER to 0,
+                Attributes.MOOD to 0
+            ),
+            TransformationEntity(0, 1, "", "d")
+        )
+        grammarHandler.addGrammarRule(languages[0]!!.grammar, rule1)
+        val rule2 = GrammarRuleEntity(
+            0, MascEntity(
+                PartOfSpeech.ADVERB, mutableMapOf()
+            ), mutableMapOf(Attributes.DEGREE_OF_COMPARISON to 1),
+            TransformationEntity(0, 0, "", "d")
+        )
+        grammarHandler.addGrammarRule(languages[0]!!.grammar, rule2)
+
+        grammarHandler.addOption(
+            languages[0]!!.grammar, CharacteristicEntity(
+                languages[0]!!.grammar.nextIds[Attributes.GENDER]!!,
+                Attributes.GENDER,
+                "полосатые деревья",
+                2
             )
-            val word2 = VerbEntity(
-                0,
-                "bbb",
-                "заплакать",
-                partOfSpeech = PartOfSpeech.VERB
+        )
+        grammarHandler.addOption(
+            languages[0]!!.grammar, CharacteristicEntity(
+                languages[0]!!.grammar.nextIds[Attributes.GENDER]!!,
+                Attributes.GENDER,
+                "деревья в клеточку",
+                2
             )
-            val word3 = AdjectiveEntity(
-                0,
-                "ccc",
-                "красивый",
-                partOfSpeech = PartOfSpeech.ADJECTIVE
-            )
-            languages[0]!!.vowels = "a"
-            languages[0]!!.consonants = "b c d"
-            dict.addWord(languages[0]!!.dictionary, word1)
-            dict.addWord(languages[0]!!.dictionary, word2)
-            dict.addWord(languages[0]!!.dictionary, word3)
-            dict.addWord(
-                languages[0]!!.dictionary, AdverbEntity(
-                    0,
-                    "ddd",
-                    "красиво"
-                )
-            )
-
-            val grammarHandler = GrammarDaoImpl()
-            val rule = GrammarRuleEntity(
-                0, MascEntity(
-                    PartOfSpeech.NOUN, mutableMapOf(Attributes.GENDER to 1)
-                ), mutableMapOf(Attributes.NUMBER to 1, Attributes.CASE to 0),
-                TransformationEntity(0, 1, "", "b")
-            )
-            grammarHandler.addGrammarRule(languages[0]!!.grammar, rule)
-            val rule1 = GrammarRuleEntity(
-                0, MascEntity(
-                    PartOfSpeech.VERB, mutableMapOf()
-                ), mutableMapOf(
-                    Attributes.NUMBER to 1,
-                    Attributes.PERSON to 0,
-                    Attributes.TIME to 0,
-                    Attributes.GENDER to 0,
-                    Attributes.MOOD to 0
-                ),
-                TransformationEntity(0, 1, "", "d")
-            )
-            grammarHandler.addGrammarRule(languages[0]!!.grammar, rule1)
-            val rule2 = GrammarRuleEntity(
-                0, MascEntity(
-                    PartOfSpeech.ADVERB, mutableMapOf()
-                ), mutableMapOf(Attributes.DEGREE_OF_COMPARISON to 1),
-                TransformationEntity(0, 0, "", "d")
-            )
-            grammarHandler.addGrammarRule(languages[0]!!.grammar, rule2)
-
-            grammarHandler.addOption(
-                languages[0]!!.grammar, CharacteristicEntity(
-                    languages[0]!!.grammar.nextIds[Attributes.GENDER]!!,
-                    Attributes.GENDER,
-                    "полосатые деревья",
-                    2
-                )
-            )
-            grammarHandler.addOption(
-                languages[0]!!.grammar, CharacteristicEntity(
-                    languages[0]!!.grammar.nextIds[Attributes.GENDER]!!,
-                    Attributes.GENDER,
-                    "деревья в клеточку",
-                    2
-                )
-            )
-            WritingDaoImpl().addCapitalizedPartOfSpeech(languages[0]!!, PartOfSpeech.NOUN)
-            PunctuationDaoImpl().updatePunctuationSymbol(languages[0]!!, 0, "MEOW");
-        }*/
-
-
-
-
-
-
-
-
+        )
+        WritingDaoImpl().addCapitalizedPartOfSpeech(languages[0]!!, PartOfSpeech.NOUN)
+        PunctuationDaoImpl().updatePunctuationSymbol(languages[0]!!, 0, "MEOW")
+        */
 
         //button new lang listener
         val buttonNewLang: Button = findViewById(R.id.buttonNewLang)
@@ -187,11 +188,7 @@ class MainActivity : AppCompatActivity() {
         listLanguages.onItemClickListener =
             AdapterView.OnItemClickListener { parent, itemClicked, position, id ->
                 val intent = Intent(this@MainActivity, LanguageActivity::class.java)
-                Log.d("meowmeow", "pos $position real ${languages.values.toList()[position].languageId}")
                 intent.putExtra("lang", languages.values.toList()[position].languageId)
-                //LanguageDaoImpl().downloadLanguagePDF(languages.values.toList()[position], storageHelper, createPDFLauncher)
-                //LanguageDaoImpl().downloadLanguageJSON(languages.values.toList()[position], storageHelper, createJSONLauncher)
-                //LanguageDaoImpl().getLanguageFromFile(DocumentFileCompat.getAccessibleAbsolutePaths(this).values.toList()[0].toList()[0]+"/1.json", this)
                 startActivity(intent)
             }
     }
