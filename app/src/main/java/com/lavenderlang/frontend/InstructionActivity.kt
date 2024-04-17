1ed0db40128c93d80b854cfacbdb92e79bd85e88
Elizabeth <arisovaliza@gmail.com>
2024-04-17 16:35:51 +0300

    minor changes: information

--- app/src/main/java/com/lavenderlang/frontend/InstructionActivity.kt
+++ app/src/main/java/com/lavenderlang/frontend/InstructionActivity.kt
@@ -3,12 +3,14 @@ package com.lavenderlang.frontend
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
 import com.lavenderlang.R
 
 class InstructionActivity: AppCompatActivity() {
     companion object{
         var id_lang: Int = 0
+        var id_block: Int = 0
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,9 @@ class InstructionActivity: AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         id_lang = intent.getIntExtra("lang", -1)
+        id_block = intent.getIntExtra("block", -1)
+        val list: ScrollView = findViewById(R.id.scrollView2)
+
     }
 
     override fun finish() {
